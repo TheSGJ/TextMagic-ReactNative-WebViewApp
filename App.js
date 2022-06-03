1280be0f1c07dd2aab29fc52d0bb73f72dd957b7
Shri Ganesh Jha <realsgj@gmail.com>
2022-06-03 22:15:48 +0000

    Initial Commit

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview'
 
+const TEXTMAGIC = 'https://text-magic-react.vercel.app/'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{width:'100%', height:'100%'}}>
+        <WebView 
+          source={{uri: TEXTMAGIC}}
+          onLoad={console.log("Loaded the WebView!")}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,8 +19,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 28,
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#222',
     alignItems: 'center',
     justifyContent: 'center',
   },
